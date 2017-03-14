623e940a83d24b8adfbad55b7a52212789027633
marek.siebert <marek.siebert@b2bnetwork.pl>
2017-03-14 12:02:04 +0100

    initial commit

---
+++ marek2209/otomoto/src/main/PageFactory/UsingOtoMotoSearchPage.java
@@ -0,0 +1,17 @@
+package marek2209.selenium.otomoto;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.junit.*;
+
+public class UsingOtoMotoSearchPage {
+	
+	@Before
+	private void setup(){
+		  // Create a new instance of a driver
+	    WebDriver driver = new FirefoxDriver("webdriver.gecko.driver","C:\Users\b2bnet\Documents\seleniumTest\marek2209\otomoto\geckodriver.exe");
+	}
+	
+
+}
